a98553b47933859f0ab460038edbd977ff8450fc
Andrzej <bazylek11@gmail.com>
2026-06-02 16:12:01 +0200

    feat: InMemoryOrderRepository created

---
+++ src/main/java/com/ecommerce/repository/order/InMemoryOrderRepository.java
@@ -0,0 +1,27 @@
+package com.ecommerce.repository.order;
+
+import com.ecommerce.domain.order.Order;
+
+import java.util.*;
+
+public class InMemoryOrderRepository implements OrderRepository {
+
+    private final Map<UUID, Order> orders = new HashMap<>();
+
+    @Override
+    public void save(Order order) {
+        orders.put(order.getId(), order);
+    }
+
+    @Override
+    public Optional<Order> findById(UUID id) {
+        return Optional.ofNullable(orders.get(id));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return orders.values()
+                .stream()
+                .toList();
+    }
+}
