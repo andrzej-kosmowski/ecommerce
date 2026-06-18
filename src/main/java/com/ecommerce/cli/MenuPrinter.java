6d0c1fbc2f2fd131fb6bb682e63c2ced2b0e5c2a
Andrzej <bazylek11@gmail.com>
2026-06-18 22:04:12 +0200

    refactor: change system out printl to lombok logger

--- src/main/java/com/ecommerce/cli/MenuPrinter.java
+++ src/main/java/com/ecommerce/cli/MenuPrinter.java
@@ -1,14 +1,17 @@
 package com.ecommerce.cli;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 
+@Slf4j
 public class MenuPrinter {
     public static void printMenu() {
         System.out.println("\n===== E-COMMERCE =====");
 
         Arrays.stream(MenuOption.values())
                 .forEach(option ->
-                        System.out.printf("%s. %s%n", option.getNumber(), option.getLabel())
+                        log.info("{} {}\n", option.getNumber(), option.getLabel())
                 );
     }
 }
